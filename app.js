849db644dc423c2540e7541b9f4e0fd1e1e25f23
Christian <cjimenez13@outlook.com>
2016-02-22 17:40:38 -0600

    ES6 Javascript

--- app.js
+++ app.js
@@ -1,18 +1,77 @@
-function Bear(type) {
-  this.type = type
+
+ 
+class Bear {
+  constructor () {
+    this.type = 'bear'
+  }
+  says (say) {
+    console.log(this.type + ' says ' + say)
+  }
 }
-Bear.prototype.growl = function() {
-  console.log('The ' + this.type + ' bear says grrr')
+
+class Grizzly extends Bear {
+  constructor () {
+    super()
+    this.type = 'grizzly'
+  }
 }
 
-function Grizzly() {
-  Bear.call(this, 'grizzly')
+let bear = new Grizzly()
+bear.says('growl')
+
+
+/*/*
+ * arrow functions
+ */
+let bears = ['polar', 'koala'].filter((bear) => {
+  return bear !== 'koala'
+})
+console.log(bears)
+
+class Bear {
+  constructor () {
+    this.type = 'bear'
+  }
+  says (say) {
+    setTimeout(() => {
+      console.log(this.type + ' says ' + say)
+    }, 1000)
+  }
 }
-Grizzly.prototype = Object.create(Bear.prototype)
+var bear = new Bear()
+bear.says()
 
 
-var grizzly = new Grizzly()
-var polar = new Bear('polar')
 
 
-grizzly.growl()
+
+/*
+ * template strings
+ */
+let bear = 'grizzly'
+let says = 'growl'
+console.log(`The ${bear} says ${says}`)
+
+
+/*
+ * destructuring
+ */
+let bear = 'grizzly'
+let says = 'growl'
+let zoo = { bear, says }
+//console.log(zoo)
+
+let grizzly = { type: 'grizzly', many: 2 }
+let { type, many } = grizzly
+console.log(many, type)
+
+
+
+
+/*
+ * rest arguments
+ */
+function bears (...types) {
+  console.log(types)
+}
+bears('polar', 'grizzly', 'brown')
